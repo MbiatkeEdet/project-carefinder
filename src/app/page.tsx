ca7f4be160c80852f20c643099d516b414bbdc22
Mbiatke Nkanta <mbiatke.nkanta@gmail.com>
2024-08-30 01:22:25 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-no-comment-textnodes */
 
 import React from 'react'
 import Link from 'next/link'
@@ -8,7 +7,7 @@ import Image from 'next/image'
 import hospital7 from '/public/hospital7.jpg'
 import hospitallogin from '/public/hospital-login.jpg'
 
-function Home({ children }: { children: React.ReactNode }) {
+function Home( children? : { children: React.ReactNode }) {
   return (
      <div className="max-w 4xl mx-auto py-2">
       <h1 className="font-semibold text-14xl h-40 mt-7">Home</h1>
@@ -16,26 +15,25 @@ function Home({ children }: { children: React.ReactNode }) {
         <div className="relative">
           <Image layout="responsive"src={hospital7} width={422} height={123} alt="display-icon"/>
         </div>
-// eslint-disable-next-line react/jsx-no-comment-textnodes
-//         <div>
-//         <h2 className="font-light text-center text-6xl text-blue-600">Finding the right medical care is crucial for your well-being and peace of mind</h2>
+        <div>
+         <h2 className="font-light text-center text-6xl text-blue-600">Finding the right medical care is crucial for your well-being and peace of mind</h2>
 
-// <p className="font-light text-center text-4xl mt-4 text-blue-600">At care-finder, we simplify the process of locating high quality healthcare services tailored to your specific needs. Our platform connects you with trusted providers,ensuring you receive the best care possible</p>
-//         </div>
+ <p className="font-light text-center text-4xl mt-4 text-blue-600">At care-finder, we simplify the process of locating high quality healthcare services tailored to your specific needs. Our platform connects you with trusted providers,ensuring you receive the best care possible</p>
+         </div>
          
-//       </div>
-//       <div>
-//       </div>
-//       <div>
-//         <section className="bg-White py-20 mt-20">
-//         <div className="sm:w-3/4 lg:w-5/12 mx-auto px-2">
-//           <h1 className="text-3xl text-center text-Grey/800 font-semibold md: text-4">
-//             Gain access to the most up-to-date and reliable hospital information
-//             in Nigeria.
-//           </h1>
-//           <div className="flex flex-col rounded-md shadow-md">
-//             <div className="p-6 flex flex-col items-center hover:bg-Grey/200">
-//               <Image
+       </div>
+       <div>
+       </div>
+       <div>
+         <section className="bg-White py-20 mt-20">
+         <div className="sm:w-3/4 lg:w-5/12 mx-auto px-2">
+           <h1 className="text-3xl text-center text-Grey/800 font-semibold md: text-4">
+             Gain access to the most up-to-date and reliable hospital information
+             in Nigeria.
+          </h1>
+        <div className="flex flex-col rounded-md shadow-md">
+          <div className="p-6 flex flex-col items-center hover:bg-Grey/200">
+             <Image
                 src={hospital7} width={422} height={123}
                 alt="download-icon"
               />
@@ -47,30 +45,30 @@ function Home({ children }: { children: React.ReactNode }) {
                 later. Carefinder allows you to download and share, making it
                 convenient to revisit them whenever you need.
               </p>
-//             </div>
-//           </div>
-//           {/* card 2 */}
-//           <div className="flex flex-col p-8 rounded-md shadow-md">
-//             <div className="p-8 flex flex-col items-center hover:bg-Grey/200">
-//               <Image layout="responsive"src={hospitallogin} width={422} height={232} alt="search-con" />
-//               <h3 className="mt-5 mb-2 text-Grey/800 text-lg font-semibold">
-//                 Advanced Search
-//               </h3>
-//               <p className="text-Grey/600 font-light text-center">
-//                 Our intuitive search filters allow you to narrow down your
-//                 hospital search based on location, medical specialization,
-//                 available services, and more. With just a few clicks, You will
-//                 find the hospitals that best match your needs.
-//               </p>
-//             </div>
-//           </div>
-//         </div>
+             </div>
+           </div>
+           {/* card 2 */}
+           <div className="flex flex-col p-8 rounded-md shadow-md">
+             <div className="p-8 flex flex-col items-center hover:bg-Grey/200">
+               <Image layout="responsive"src={hospitallogin} width={422} height={232} alt="search-con" />
+               <h3 className="mt-5 mb-2 text-Grey/800 text-lg font-semibold">
+                 Advanced Search
+               </h3>
+               <p className="text-Grey/600 font-light text-center">
+                 Our intuitive search filters allow you to narrow down your
+                 hospital search based on location, medical specialization,
+                 available services, and more. With just a few clicks, You will
+                 find the hospitals that best match your needs.
+               </p>
+             </div>
+           </div>
+         </div>
         
-//         </section>
+         </section>
         
-//       </div>
+       </div>
       
-//      </div>
+    </div>
     
   );
 }
